0bc936a9e33d83d345fa6c23d8ebe961484f9fe0
ZannaC <zhanna_s1@inbox.lv>
2022-05-16 21:53:54 +0300

    Add new feature/java branch

---
+++ src/main/java/classroom/MyFirstJavaCode.java
@@ -0,0 +1,35 @@
+package classroom;
+
+public class MyFirstJavaCode {
+    public static void main(String[] args) {
+/*multiline comment*/
+   byte floarCount = 3;
+        System.out.println("My house has "+ floarCount + " floars");
+        System.out.printf("My house has %d floars", floarCount);
+        // %d numbers
+        // %s text/string
+        //  boolean
+        short salaryInEur = 1000;
+        System.out.println(salaryInEur);
+        System.out.println(1000);
+        System.out.printf("average salary for junior QA Engineer is: %d ", salaryInEur);
+        int abcdf = 145263;
+        long curentworld = 12345;
+        System.out.println("abcdf "+ curentworld + "");
+        double gasPriceInEur = 3.876;
+        System.out.println(gasPriceInEur);
+        //string myName = "Zanna";
+       // System.out.println(myName);
+
+        int a = 3;
+        double residentCount = 15.0;
+        System.out.printf("In my house %f live", residentCount);
+        System.out.printf("In my house %.1f live", residentCount);
+//is/has
+        boolean isSummer = false;
+        boolean isSpring = true;
+        System.out.println(isSummer);
+        System.out.println(isSpring);
+
+    }
+}
